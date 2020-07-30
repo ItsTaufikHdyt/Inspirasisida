d3a0757f6be069957f1d231ab6fc13da30d13e8c
Taufik Hidayat <taufikhdyt2332@gmail.com>
2020-07-30 10:25:48 +0800

    Membuat Tampilan Database Penelitian & Inovasi

--- resources/views/admin/database/index.blade.php
+++ resources/views/admin/database/index.blade.php
@@ -7,88 +7,176 @@
 <h4>Database Penelitian dan Inovasi</h4>
 <div class="row">
 	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-	    <div class="sparkline13-graph">
-	        <div class="datatable-dashv1-list custom-datatable-overright">
-	            <div id="toolbar">
-                    <button type="button" class="btn btn-custon-four btn-primary" data-toggle="modal" data-target="#PrimaryModalalert">
-						<i class="fa fa-plus-circle"></i> Input Data
-					</button>
-					<div id="PrimaryModalalert" class="modal modal-edu-general default-popup-PrimaryModal fade" role="dialog">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                            	<form method="post" enctype="multipart/form-data">
-                                <div class="modal-close-area modal-close-df">
-                                    <a class="close" data-dismiss="modal" href="#"><i class="fa fa-close"></i></a>
+	<div class="product-payment-inner-st res-mg-t-30 analysis-progrebar-ctn">
+            <ul id="myTabedu1" class="tab-review-design">
+                <li class="active"><a data-toggle="tab" href="#TabPerangkatDaerah">Perangkat Daerah</a>
+                </li>
+                <li><a data-toggle="tab" href="#TabMasyarakat">Masyarakat</a>
+                </li>
+            </ul>
+            <div id="myTabContent" class="tab-content custom-product-edit st-prf-pro">
+                <div class="product-tab-list tab-pane fade active in" id="TabPerangkatDaerah">
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <div class="review-content-section">
+                                <div class="row">
+                                    <div class="col-lg-12">
+										<div class="bs-bars pull-left">
+											<button type="button" class="btn btn-custon-four btn-primary" data-toggle="modal" data-target="#perangkatDaerah">
+												<i class="fa fa-plus-circle"></i> Input Data
+											</button>
+										</div>
+										<div id="perangkatDaerah" class="modal modal-xl  fade" role="dialog">
+											<div class="modal-dialog">
+												<div class="modal-content">
+													<form action="#" method="post">
+													@csrf
+													<div class="modal-close-area modal-close-df">
+														<a class="close" data-dismiss="modal" href="#"><i class="fa fa-close"></i></a>
+													</div>
+													<div class="modal-body">
+														<table width="100%">
+															<tr>
+																<td>Judul</td>
+															</tr>
+															<tr>
+																<td><input type="text" name="judul" class="form-control" placeholder="Judul Inovasi / Penelitian..." required></td>
+															</tr>
+															<tr>
+																<td>Tahun</td>
+															</tr>
+															<tr>
+																<td><input type="text" name="tahun" class="form-control" placeholder="Tahun..." required></td>
+															</tr>
+															<tr>
+																<td>Perangkat Daerah</td>
+															</tr>
+															<tr>
+																<td><input type="text" name="opd" class="form-control" placeholder="Perangkat Daerah..." required></td>
+															</tr>
+															<tr>
+																<td>Abstraksi / Profil Inovasi</td>
+															</tr>
+															<tr>
+																<td>
+																	<!-- <div id="summernote2" name="narasi">
+																	</div> -->
+																<textarea id="abstraksi"  name="narasi" cols="60" rows="10" placeholder="Abstraksi..." required></textarea>
+																</td>
+															</tr>
+															<tr>
+																<td>Kategori</td>
+															</tr>
+															<tr>
+																<td>
+																<select name="kategori" class="form-control" id="">
+																	<option value="inovasi">Inovasi</option>
+																	<option value="penelitian">Penelitian</option>
+																</select>
+																</td>
+															</tr>
+														</table>
+													</div>
+													<div class="modal-footer">
+														<button type="button" class="btn btn-custon-four btn-default btn-md" data-dismiss="modal"><i class="fa fa-close"></i> Cancel</button>
+														<button type="submit" name="simpan" class="btn btn-custon-four btn-primary btn-md"><i class="fa fa-save"></i> Save</button>
+													</div>
+													</form>
+												</div>
+											</div>
+										</div>
+                                    	<table id="table" data-toggle="table" data-pagination="true" data-search="true" data-show-columns="true" data-show-pagination-switch="true" data-show-refresh="true" data-key-events="true" data-show-toggle="true" data-resizable="true" data-cookie="true"
+	                data-cookie-id-table="saveId" data-show-export="true" data-click-to-select="true" data-toolbar="#toolbar">
+                                    		<thead>
+							                    <tr>
+							                        <th data-field="id">No.</th>
+							                        <th data-field="name">Judul</th>
+							                        <th data-field="thn">Tahun</th>
+							                        <th data-field="opd">Perangkat Daerah</th>
+							                        <th data-field="location">Lokasi Kegiatan</th>
+													<th data-field="abstraksi">Abstraksi/Profil Inovasi</th>
+													<th data-field="category">Kategori</th>
+							                        <th data-field="action">Aksi</th>
+							                    </tr>
+							                </thead>
+							                <tbody>
+                                            
+								                <tr>
+								                						                    
+	                                    			<td style="width: 5px;"></td>
+	                                    			<td></td>
+	                                    			<td></td>
+	                                    			<td></td>
+													<td></td>
+	                                    			<td></td>
+													<td></td>
+	                                    			
+	                                    		</tr>                                               
+	                                    	</tbody>
+	                                    </table>
+                                    </div>
                                 </div>
-                                <div class="modal-body">
-                                	<table width="100%">
-                                		<tr>
-                                			<td>Upload File</td>
-                                		</tr>
-                                		<tr>
-                                			<td>
-                                				<div class="file-upload-inner ts-forms">
-                                                    <div class="input prepend-small-btn">
-                                                        <div class="file-button">
-                                                            Browse
-                                                            <input type="file" name="file" required onchange="document.getElementById('prepend-small-btn2').value = this.value;">
-                                                        </div>
-                                                        <input type="text" name="file" id="prepend-small-btn2" placeholder="no file selected" required>
-                                                    </div>
-                                                </div>
-					                        </td>
-                                		</tr>
-                                		<tr>
-                                			<td>Deskripsi Berkas</td>
-                                		</tr>
-                                		<tr>
-                                			<td>
-                                				<input type="text" class="form-control" name="judul" required>
-                                			</td>
-                                		</tr>
-                                		<tr>
-                                			<td>Kategori Berkas</td>
-                                		</tr>
-                                		<tr>
-                                			<td>
-                                				<select class="form-control custom-select-value" name="kategori" required>
-                                					<option>-Pilih Kategori-</option>
-                                					<option value="Inovasi">Inovasi</option>
-                                					<option value="Penelitian">Penelitian</option>
-                                				</select>
-                                			</td>
-                                		</tr>
-                                	</table>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-custon-four btn-default btn-md" data-dismiss="modal"><i class="fa fa-close"></i> Cancel</button>
-                                    <button type="submit" name="simpan" class="btn btn-custon-four btn-primary btn-md"><i class="fa fa-save"></i> Save</button>
-                                </div>
-                            	</form>
                             </div>
                         </div>
                     </div>
                 </div>
-                <table id="table" data-toggle="table" data-pagination="true" data-search="true" data-show-columns="true" data-show-pagination-switch="true" data-show-refresh="true" data-key-events="true" data-show-toggle="true" data-resizable="true" data-cookie="true"
+				<div class="product-tab-list tab-pane fade" id="TabMasyarakat">
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <div class="review-content-section">
+                                <div class="row">
+                                    <div class="col-lg-12">
+										<div class="bs-bars pull-left">
+											<button type="button" class="btn btn-custon-four btn-primary" data-toggle="modal" data-target="#PrimaryModalalert">
+												<i class="fa fa-plus-circle"></i> Input Data
+											</button>
+										</div>
+                                    	<table id="table" data-toggle="table" data-pagination="true" data-search="true" data-show-columns="true" data-show-pagination-switch="true" data-show-refresh="true" data-key-events="true" data-show-toggle="true" data-resizable="true" data-cookie="true"
 	                data-cookie-id-table="saveId" data-show-export="true" data-click-to-select="true" data-toolbar="#toolbar">
-	                <thead>
-	                    <tr>
-	                        <th data-field="id">No.</th>
-	                        <th data-field="name">Tanggal Upload</th>
-	                        <th data-field="berkas" width="30%">Nama Berkas</th>
-	                        <th data-field="judul" width="30%">Deskripsi Berkas</th>
-	                        <th data-field="kategori" width="30%">Kategori Berkas</th>
-	                        <th data-field="action">Aksi</th>
-	                    </tr>
-	                </thead>
-	                <tbody>
-	                    <tr>
-							<td colspan="6" align="center">Data Belum Tersedia</td>
-	                    </tr>
-	                </tbody>
-	            </table>
-	        </div>
-	    </div>
+                                    		<thead>
+							                    <tr>
+							                        <th data-field="id">No.</th>
+							                        <th data-field="name">Judul</th>
+							                        <th data-field="opd">Nama Peserta</th>
+							                        <th data-field="location">Lokasi Kegiatan</th>
+													<th data-field="abstraksi">Kriteria Peserta</th>
+							                        <th data-field="thn">Kategori</th>
+							                        <th data-field="action">Aksi</th>
+							                    </tr>
+							                </thead>
+							                <tbody>
+                                            
+								                <tr>
+								                						                    
+	                                    			<td style="width: 5px;"></td>
+	                                    			<td></td>
+	                                    			<td></td>
+	                                    			<td></td>
+													<td></td>
+	                                    			<td></td>
+													<td></td>
+	                                    			
+	                                    		</tr>                                               
+	                                    	</tbody>
+	                                    </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+			</div>
+
+        </div>
     </div>
 </div>
+@endsection
+@section('custom_scripts')
+<script type="text/javascript">
+  var konten = document.getElementById("abstraksi");
+    CKEDITOR.replace(konten,{
+    language:'en-gb'
+  });
+  CKEDITOR.config.allowedContent = true;
+</script>
 @endsection
